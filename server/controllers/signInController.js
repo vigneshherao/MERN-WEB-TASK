02c7e60c1cc6f56d11103bddd6d7d0487ceb3320
vignesh <vigneshfornavy@gmail.com>
2024-08-08 14:52:35 +0530

    Implementaion of SignIn Functionality

---
+++ server/controllers/signInController.js
@@ -0,0 +1,35 @@
+const User = require("../models/user");
+const bcrpyt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+//Here it will check status
+
+const signin = async (req, res) => {
+  try {
+    const {email, password } = req.body;
+    const user = await User.findOne({ email });
+    // if the user not found in database
+    if (!user) {
+      return res
+        .status(403)
+        .json({ message: "User not Found, Create One", sucess: false });
+    }
+    
+    //comparing the password from database if it is not matching show error
+    const isPswd = await bcrpyt.compare(password,user.password);
+    if(!isPswd){
+        return res
+        .status(403)
+        .json({ message: "Password dosen't match", sucess: false });
+    }
+
+    //creating jwtToken for the users loggin
+    const jwtToken = jwt.sign({email:user.email,_id:user._id},process.env.JWT_SECRET,{expiresIn:'24h'});
+    res.status(200).json({ message: "signin sucessfull", sucess: true,jwtToken,email,name:user.name});
+  } catch (err) {
+    res.status(500).json({ message: `signin failed ${err}`, sucess: false });
+  }
+};
+
+module.exports = signin;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const {signUpValidation,signInValidation}  = require("../middleware/userValidation")
 const signUp = require("../controllers/signUpController");
+const signIn = require("../controllers/signInController")
 
 
 router.post("/signup",signUpValidation,signUp);
 
-router.post("/signin",signInValidation,(req,res)=>{
-    res.send("signin");
-})
+router.post("/signin",signInValidation,signIn);
 
 
 
