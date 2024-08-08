fb18cfdec9d2c72f8a1ed3ca31c531e0d15e81db
vignesh <vigneshfornavy@gmail.com>
2024-08-08 11:39:20 +0530

    inital commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-red-500 text-3xl">Hello</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hello");
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
+
+
