a5dcc06d4769a0becfb645f4a2dfb523cb2bc02f
vignesh <vigneshfornavy@gmail.com>
2024-08-10 20:11:51 +0530

    update

--- server/models/db.js
+++ server/models/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 //connection to database
 
+const Mongo_URL = process.env.DBURL;
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/server")
+  .connect("mongodb+srv://vignesh:MSzA7aS1uoI44alb@cluster0.ao1473g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Connected to database!"))
   .catch((error) => console.log(error));
