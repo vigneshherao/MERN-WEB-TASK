0aff9c9a9c3e2eca9a9921022ecf26912583d181
vignesh <vigneshfornavy@gmail.com>
2024-08-11 21:17:04 +0530

    Updated Api

--- client/src/utils/constantApi.js
+++ client/src/utils/constantApi.js
@@ -3,7 +3,7 @@
 export const Fetch_COURSE = "https://mern-web-apis.onrender.com/list/courses";
 export const SignIN_API="https://mern-web-apis.onrender.com/user/signin";
 export const SignUP_API = "https://mern-web-apis.onrender.com/user/signup";
-export const GoogleAPI = "https://mern-web-apis.onrender.com/google-auth";
+export const GoogleAPI = "https://mern-web-apis.onrender.com/api/google-auth";
 
 
 
